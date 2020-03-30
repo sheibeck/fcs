58fa204258490de45e9dde429861ddd4f357c549
Sterling Heibeck <sheibeck@gmail.com>
2020-03-30 13:30:34 -0400

    allow for a full delete upon redeploy

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -114,7 +114,19 @@ const webpackConfig = merge(baseWebpackConfig, {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
-      }
+      },
+      { 
+        from: path.resolve(__dirname, '../robots.txt'),
+        to:'.'
+      },
+      { 
+        from: path.resolve(__dirname, '../favicon.png'),
+        to:'.'
+      },
+      { 
+        from: path.resolve(__dirname, '../favicon.ico'),
+        to:'.'
+      } 
     ])
   ]
 })
