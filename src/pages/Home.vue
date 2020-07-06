255508703577e1bf4258aee2250f70052b370b85
Sterling Heibeck <sheibeck@gmail.com>
2020-07-05 23:19:36 -0400

    Add verbiage for scene builder information.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -7,14 +7,25 @@
   </div>
 
   <div class="row">
-        <div v-if="!HasSubscription" class="col-sm-6">
+        <div v-if="!isSubscriber" class="col-sm-6">
           <div class="card card m-2">
             <div class="card-body">
               <h5 class="card-title">Become a Subscriber</h5>
-              <p class="card-text">Subscribing will give you access to great features like Roll20 integration. Register for free and get your game on!</p>
-              <a v-if="!isAuthenticated" href="register" class="btn btn-primary">Create a Free Account <span class='dice'>C</span></a>
+              <p class="card-text">Register for free. Subscribe to get access to Scene builder, our Fate virtual table top (Players don't need a subscription to play.) and Roll20 integration.</p>
+              <a v-if="!isAuthenticated" href="register" class="btn btn-primary"><span class='dice'>C</span> Create a Free Account</a>
               <a v-if="isAuthenticated" href="account" type="button" class="btn btn-primary">
-                  Become a Subscriber <span class='dice'>C</span>
+                  <span class='dice'>C</span> Become a Subscriber
+              </a>
+            </div>
+          </div>
+        </div>
+        <div v-if="isSubscriber" class="col-sm-6">
+          <div class="card card m-2">
+            <div class="card-body">
+              <h5 class="card-title">Build Your Scenes &amp; Play Online</h5>
+              <p class="card-text">Build scenes for your fate sessions and import your existing characters and adversaries, then play online. Players don't need a subscription to play!</p>
+              <a v-if="isAuthenticated" href="scene" type="button" class="btn btn-primary">
+                  <i class="fas fa-book-open"></i> Build &amp; Play Online
               </a>
             </div>
           </div>
@@ -24,9 +35,9 @@
             <div class="card-body">
               <h5 class="card-title">View Your Fate Characters</h5>
               <p class="card-text">Create and manage all of your fate characters online. You can store and edit as many characters as you want using any of our Fate Character Sheets.</p>
-              <a href="character" class="btn btn-primary requires-auth hidden">Play a Fate Character <i class="fas fa-user"></i></a>
+              <a href="character" class="btn btn-primary requires-auth hidden"><i class="fas fa-user"></i> Play a Fate Character</a>
               <a href="login" type="button" class="btn btn-primary requires-noauth">
-                  Login to View Your Characters <i class="fas fa-user"></i>
+                  <i class="fas fa-user"></i> Login to View Your Characters
               </a>
             </div>
           </div>
@@ -35,10 +46,10 @@
           <div class="card card m-2">
             <div class="card-body">
               <h5 class="card-title">View Your Campaigns</h5>
-              <p class="card-text">Create and manage a campaign setting. Record session notes and share them with your players. Build your world and start playing!</p>
-              <a href="campaign" class="btn btn-primary requires-auth hidden">Play a Campaign <i class="fas fa-globe-americas"></i></a>
+              <p class="card-text">Create and manage multiple campaign settings. Record session notes and share them with your players. Build your world and start playing!</p>
+              <a href="campaign" class="btn btn-primary requires-auth hidden"><i class="fas fa-globe-americas"></i> Play a Campaign</a>
               <a href="login" type="button" class="btn btn-primary requires-noauth">
-                  Login to View Your Campaigns <i class="fas fa-globe-americas"></i>
+                <i class="fas fa-globe-americas"></i> Login to View Your Campaigns
               </a>
             </div>
           </div>
@@ -48,7 +59,7 @@
             <div class="card-body">
               <h5 class="card-title">Find a Fate Character Sheet</h5>
               <p class="card-text">Find a character sheet for the setting you want. We have form-fillable Fate Core, Fate Accelerated and other setting character sheets.</p>
-              <a href="charactersheet" class="btn btn-primary">Find a Character Sheet <span class='dice'>D</span></a>
+              <a href="charactersheet" class="btn btn-primary"><span class='dice'>D</span> Find a Character Sheet</a>
             </div>
           </div>
         </div>
@@ -57,7 +68,7 @@
             <div class="card-body">
               <h5 class="card-title">View the Adversary Codex</h5>
               <p class="card-text">View a codex of premade fate adversaries. If you can't find an adversary that you like, you can create your own and share it with the community.</p>
-              <a href="adversary" class="btn btn-primary">Fight a Fate Adversary <span class='dice'>A</span></a>
+              <a href="adversary" class="btn btn-primary"><span class='dice'>A</span> Build &amp; Play an Adversary</a>
             </div>
           </div>
         </div>
@@ -66,7 +77,7 @@
             <div class="card-body">
               <h5 class="card-title">Need Some Fate Dice?</h5>
               <p class="card-text">Forgot your dice? Want to keep track of all your session rolls? We've got you covered! Use our fate dice roller to cover your game play needs.</p>
-              <a href="#" data-toggle='modal' data-target='#modalDiceRoller' class="btn btn-primary">Roll Fate Dice <span class='dice'>+</span></a>
+              <a href="#" data-toggle='modal' data-target='#modalDiceRoller' class="btn btn-primary"><span class='dice'>+</span> Roll Fate Dice</a>
             </div>
           </div>
         </div>
@@ -91,10 +102,8 @@ export default {
     ...mapGetters([
       'isAuthenticated',
       'userId',
-    ]),
-    HasSubscription() {
-      return this.$store.state.hasActiveSubscription;
-    },
+      'isSubscriber'
+    ])  
   },
 }
 </script>
